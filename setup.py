498bcd1fdfbb51ec704538a779330dfab826f51a
Stephen Tu <stephentu@csail.mit.edu>
2014-07-13 00:25:12 -0700

    travis CI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='passgen',
+      version='1.0',
+      description='generate random passwords',
+      author='Stephen Tu',
+      author_email='steve33671@gmail.com',
+      packages=['passgen'],
+     )
